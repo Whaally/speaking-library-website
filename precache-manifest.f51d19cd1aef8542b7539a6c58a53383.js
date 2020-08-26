0a7cff0cf083721383eb102a2c4d9b090664ef6e
Corstian Boerman <mail@corstianboerman.com>
2020-08-26 14:07:13 +0200

    Updates

---
+++ precache-manifest.f51d19cd1aef8542b7539a6c58a53383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744acedab3d83d4eeb9021f268648156",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed203c98a7ba4f9eefba",
+    "url": "/static/css/main.eb6e28ce.chunk.css"
+  },
+  {
+    "revision": "d0d884436829ec2cb2d2",
+    "url": "/static/js/2.4eb2ce28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4eb2ce28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed203c98a7ba4f9eefba",
+    "url": "/static/js/main.7984c153.chunk.js"
+  },
+  {
+    "revision": "60568c5f3787c121b29e",
+    "url": "/static/js/runtime-main.e17ad240.js"
+  }
+]);
